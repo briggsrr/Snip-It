7ca96449e4f03e3ff45795a32db3f1141220fc9e
briggsrr <robert.briggs.3@icloud.com>
2022-09-14 21:53:27 -0700

    Huge serverside changes: added like, delete, update

--- server/index.js
+++ server/index.js
@@ -14,8 +14,10 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://rbriggstemp:temppassword@cluster0.py2iwzi.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT|| 5002;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
+
+//mongoose.set('useFindAndModify', false);
